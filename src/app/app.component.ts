c3243a6be9efa6debbfdfb4cdd5f8f559af171b5
Mateo Cordoba <matucorneron@gmail.com>
2025-04-20 00:54:41 -0300

    agregar carpetas components, models y services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TestComponent } from "./components/test/test.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
