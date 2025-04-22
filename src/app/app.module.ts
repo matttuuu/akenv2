9719e3af8d3723c721ce3aea53ec3d69ed39fc56
Mateo Cordoba <matucorneron@gmail.com>
2025-04-21 22:40:36 -0300

    integrar firebase, crear carpetas de componentes y metodo register en auth.service.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,24 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideFirebaseApp(() => initializeApp({"projectId":"dashboardamekdev2","appId":"1:255865880746:web:59349d6d74727602a0d0d3","storageBucket":"dashboardamekdev2.firebasestorage.app","apiKey":"AIzaSyBhrcg-VnHKiFcpjFzM2njv_IXsz0C921I","authDomain":"dashboardamekdev2.firebaseapp.com","messagingSenderId":"255865880746"})),
+    provideAuth(() => getAuth())
   ],
   bootstrap: [AppComponent]
 })
